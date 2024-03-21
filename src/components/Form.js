eef9dd0dcff323c8de0ff73fcaf23620bb317d86
himanshu chandrashekhar sachan <81224933+Himanshu882k@users.noreply.github.com>
2024-03-21 13:54:26 +0530

    Mywebsite

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,40 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import "./FormStyles.css"
 
-import React from 'react'
-
 const Form = () => {
-  return (
-    <div className="form">
-        <form>
-            <label>Your Name</label>
-            <input type="text"></input>
-            <label>Email</label>
-            <input type="text"></input>
-            <label>Subject</label>
-            <input type="text"></input>
-            <label>Message</label>
-            <textarea rows="6" placeholder="Type your message here"/>
-            <button className="btn">Submit</button>
-        </form>
-    </div>
-  )
-}
+  const form = useRef();
 
-export default Form
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_ihbdi28', 'template_as07f13', form.current, {
+        publicKey: 'W7xY7LoEgaQd56Suo',
+      })
+      .then(
+        (result) => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label className='form'>Name</label>
+      <input type="text" name="user_name" />
+      <label className='form'>Email</label>
+      <input type="email" name="user_email" />
+      <label className='form'>Subject</label>
+      <input type="text" name="Subject"></input>
+      <label>Message</label>
+      <textarea rows="6" name='message' placeholder="Type your message here"/>
+      <input className='btn' type="submit" value="Send" />
+    </form>
+  );
+};
+
+export default Form;
